1612bd41d58f2050a07180440002cd07596496ac
Yankeeland <jake.davis@gmail.com>
2019-10-29 22:16:41 +1300

    Added Pycharm
    Added Pycharm project files

---
+++ main.py
@@ -0,0 +1,40 @@
+#Filename main.py
+#Author: Jake Davis<jake.davis@gmail.com>
+
+#Scope description for this project can be found at: https://github.com/Yankeeland/duplicatePhotoRemover/blob/master/README.md
+#===================================
+
+import os, send2trash
+
+#Load the UI
+
+
+
+
+
+
+#Input a directory to scan
+directoryToScan = input("Please enter a directory to scan:")
+
+
+#Scan the directory looking for duplicate images
+#load all image files into a dictionary with unique atributes for comparing
+#scan through each file comparing for duplicates
+#when a duplicate is found, present the user with a choice of which to keep, the choice should show the originating directory
+
+
+#Present duplicates one at a time and ask the user to decide which to keep and which to put into trash
+
+
+
+
+#show all done
+
+
+
+
+#go back to the beginning
+
+
+
+
