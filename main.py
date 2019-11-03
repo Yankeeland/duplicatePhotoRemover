64aa0ce1e71cff158fe45663c67548439d43a87d
Yankeeland <jake.davis@gmail.com>
2019-11-03 21:39:15 +1300

    Directory Reading
    Added code for reading the input directory. Also added test photo directory.

--- main.py
+++ main.py
@@ -4,21 +4,41 @@
 #Scope description for this project can be found at: https://github.com/Yankeeland/duplicatePhotoRemover/blob/master/README.md
 #===================================
 
-import os, send2trash
+import os
+#send2trash
 
 #Load the UI
 
+DEBUG = True
 
 
-
-
+if DEBUG:
+    print('Current working directory: ' + os.getcwd())
 
 #Input a directory to scan
-directoryToScan = input("Please enter a directory to scan:")
+#check to make sure that the directory is valid
+directoryToScan = 'NOTAPATH'
+while not os.path.isdir(os.path.abspath(directoryToScan)):
+    directoryToScan = input("Please enter a directory to scan:")
 
+directoryToScan = os.path.abspath(directoryToScan)
+os.chdir(directoryToScan)
+if DEBUG:
+    print('Current working directory: ' + os.getcwd())
+if DEBUG:
+    print('Directory found is: ' + directoryToScan)
+#Scan the directory looking for image files
+for filename in os.listdir(directoryToScan):
 
-#Scan the directory looking for duplicate images
+    #skip directories
+    if os.path.isfile(filename):
+        if DEBUG: print('Yeah! We have a file called: ' + filename)
+
+    #if the filename is a .jpg, add it to the dictionary
+
 #load all image files into a dictionary with unique atributes for comparing
+
+
 #scan through each file comparing for duplicates
 #when a duplicate is found, present the user with a choice of which to keep, the choice should show the originating directory
 
