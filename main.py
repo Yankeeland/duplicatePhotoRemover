f9ac7d6285420b4231ff3278a3c3656408e3def3
Yankeeland <jake.davis@gmail.com>
2019-11-06 22:42:30 +1300

    continued to add core logic
    Added:
    - temporary fixed directories as master and scanning for testing the program
    - logic for choosing what to do with the image

--- main.py
+++ main.py
@@ -10,7 +10,34 @@ import os
 #Load the UI
 
 DEBUG = True
+#loading testing directories REMOTE THESE 2 LINES WHEN FINISHED
+test_path = "C:\\Users\\yanke\\OneDrive\\Documents2\\GitHub\\duplicatePhotoRemover\\photos"
+test_master = "C:\\Users\\yanke\\OneDrive\\Documents2\\GitHub\\duplicatePhotoRemover\\masters"
 
+#START OF FUNCTION DEFINITION==========================================================================
+
+#Need to change this to walk the directory, otherwise it is not possible to find duplicates
+def scanDir(directoryName):
+    picDict = {}
+    os.chdir(directoryName)
+    for filename in os.listdir(directoryName):
+        filename = os.path.abspath(filename)
+
+        # skip directories
+        if os.path.isfile(filename):
+            # if DEBUG: print('Yeah! We have a file called: ' + filename)
+
+            # if the filename is a .jpg, add it to the dictionary
+            if '.jpg' in filename:
+                picName = os.path.basename(filename)
+                picPath = os.path.dirname(filename)
+                picSize = os.path.getsize(filename)
+                if picName not in picDict.keys():
+                    picDict[picName] = [picPath, picSize]
+                    if DEBUG: print('PICDICT: Added picture ' + picName + ' with size of ' + str(picSize) + 'at directory ' + picPath)
+    return picDict
+
+#END OF FUNCTION DEFINITION==========================================================================
 
 if DEBUG:
     print('Current working directory: ' + os.getcwd())
@@ -20,52 +47,55 @@ if DEBUG:
 directoryToScan = 'NOTAPATH'
 masterDirectory = 'NOTAPATH'
 
-while not os.path.isdir(os.path.abspath(directoryToScan)):
-    directoryToScan = input("Please enter a directory to scan:")
-directoryToScan = os.path.abspath(directoryToScan)
-os.chdir(directoryToScan)
-if DEBUG:
-    print('Current working directory: ' + os.getcwd())
+while not os.path.isdir(os.path.abspath(directoryToScan)): #while the input is not a directory, keep asking
+    #directoryToScan = input("Please enter a directory to scan:")
+    directoryToScan = test_path #loading testing directories REMOVE THIS LINE WHEN FINISHED
+directoryToScan = os.path.abspath(directoryToScan) #make sure you get the absolute path
+
 if DEBUG:
     print('Directory found is: ' + directoryToScan)
 
 #Input the master directory
 while not os.path.isdir(os.path.abspath(masterDirectory)):
-    masterDirectory = input("Please enter a directory that contains your master copies:")
+    #masterDirectory = input("Please enter a directory that contains your master copies:")
+    masterDirectory = test_master #loading testing directories REMOVE THIS LINE WHEN FINISHED
 masterDirectory = os.path.abspath(masterDirectory)
-if DEBUG:
-    print('Current working directory: ' + os.getcwd())
+
 if DEBUG:
     print('Master directory found is: ' + masterDirectory)
 
 
-#initialize the dictionary of pics to compare
-picList = {} #originals dictionary
-dupPicList = {} #duplicates dictionary
-
-#Scan the directory looking for image files
-#Need to change this to walk the directory, otherwise it is not possible to find duplicates
-for filename in os.listdir(directoryToScan):
-
-    #skip directories
-    if os.path.isfile(filename):
-        if DEBUG: print('Yeah! We have a file called: ' + filename)
-
-    #if the filename is a .jpg, add it to the dictionary
-        if '.jpg' in filename:
-            picName = os.path.basename(filename)
-            picPath = os.path.dirname(filename)
-            picSize = os.path.getsize(filename)
-            if picName not in picList.keys():
-                picList[picName] = [picPath,picSize]
-                if DEBUG: print('PICLIST: Added picture ' + picName + ' with size of ' + str(picSize))
-            #else: #add to the duplicates dictionary
-                #dupPicList[picName] = [picPath,picSize]
-                #if DEBUG: print('DUPPICLISTAdded picture ' + picName + ' with size of ' + str(picSize))
-
-#load the master directory
+#load the directories
+picList = scanDir(directoryToScan)
+masterList = scanDir(masterDirectory)
 
 #scan through each file comparing the scanned directory to the master directory
+for picName in picList.keys():
+    if picName in masterList.keys():
+        if DEBUG: print("HOLY SMOKES! " + str(picList[picName][1]) + " " + str(masterList[picName][1]) )
+        #check to see if they are the same size
+        if picList[picName][1] == masterList[picName][1]:
+            print("Found a duplicate by the name of " + picName)
+            print("Which copy would you like to keep? (Choose an Number)")
+            choice = 0
+            done = False
+            while (not done):
+                print("1)" + picList[picName][0] + "\\" + picName)
+                print("2)" + masterList[picName][0] + "\\" + picName)
+                print("3) Skip")
+                choice = input()
+                if choice == '1':
+                    #add new picture to master file under the dir structure
+                    done = True
+                elif choice == '2':
+                    #delete the new picture (send to trash)
+                    done = True
+                elif choice == '3':
+                    # do nothing
+                    done = True
+                else:
+                    continue
+
 
 #when a duplicate is found, present the user with a choice of which to keep, the choice should show the originating directory
 
